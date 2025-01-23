ac72365369392fa8ba0e9e490dd745431279aa91
Toputech <168991681+Toputech@users.noreply.github.com>
2025-01-23 17:35:47 +0300

    Update ping.js
    const { zokou } = require("../framework/zokou");
    const moment = require("moment-timezone");
    const { default: axios } = require('axios');
    //const conf = require('../set');
    
    
    zokou({ nomCom: 'ping',
        desc: 'To check ping',
        Categorie: 'General',
        reaction: '❣️', 
        fromMe: 'true', 
    
           
      },
      async (dest, zk, commandeOptions) => {
        const { ms, arg, repondre } = commandeOptions;
        const { start} = new Date().getTime()
        return repondre('*•𝘼𝙇𝙊𝙉𝙀-𝙈𝘿• ❣️        respond speed is*\n ```' +1000000990+ '``` *m/s*') 
        const { end } = new Date().getTime()
        await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
      }
    )

--- commandes/ping.js
+++ commandes/ping.js
@@ -15,7 +15,7 @@ zokou({ nomCom: 'ping',
   async (dest, zk, commandeOptions) => {
     const { ms, arg, repondre } = commandeOptions;
     const { start} = new Date().getTime()
-    return repondre('*ALONE~𝗠𝗗 ❣️        respond speed is*\n ```' +1000000990+ '``` *m/s*') 
+    return repondre('*•𝘼𝙇𝙊𝙉𝙀-𝙈𝘿• ❣️        respond speed is*\n ```' +1000000990+ '``` *m/s*') 
     const { end } = new Date().getTime()
     await zok.sendMessage('*Pong!*\n ```' + (end - start) + '``` *ms*')
   }
