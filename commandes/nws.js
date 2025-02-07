8d4b0be0ee6040b5f86210b024f4db35f2f91c20
Toputech <168991681+Toputech@users.noreply.github.com>
2025-02-08 02:26:00 +0300

    Update nws.js
    const { zokou } = require('../framework/zokou');
    const Heroku = require('heroku-client');
    const s = require("../set");
    const axios = require("axios");
    const speed = require("performance-now");
    const { exec } = require("child_process");
    const conf = require(__dirname + "/../set");

--- commandes/nws.js
+++ commandes/nws.js
@@ -1,1 +1,7 @@
-
+const { zokou } = require('../framework/zokou');
+const Heroku = require('heroku-client');
+const s = require("../set");
+const axios = require("axios");
+const speed = require("performance-now");
+const { exec } = require("child_process");
+const conf = require(__dirname + "/../set");
